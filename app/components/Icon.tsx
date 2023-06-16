578a78443e94f0168b9c32248c4f0455027f2d96
Mioriarty <moritz.seppelt@googlemail.com>
2023-06-16 16:07:26 +0200

    fix icon for android

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -2,7 +2,8 @@ import { IconProps, Icon as KittenIcon } from "@ui-kitten/components";
 
 export const Icon: React.FC<IconProps> = (props) => {
   if (props.style != undefined && props.style.tintColor != undefined) {
-    return <KittenIcon {...props} />;
+    const style = { ...props.style, tintColor: undefined };
+    return <KittenIcon {...props} style={style} fill={props.style.tintColor} />;
   } else {
     return <KittenIcon {...props} />;
   }
